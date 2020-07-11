bcef70df68bfcfa41f03593eb2274bca42704ebd
Tamas <jonastamas2@gmail.com>
2020-07-11 13:14:37 +0100

    feat: added action types

---
+++ src/actions/types.js
@@ -0,0 +1,13 @@
+export const types = {
+  FETCH_TOURNAMENTS: 'fetchTournaments',
+  FETCH_TOURNAMENTS_SUCCESS: 'fetchTournamentsSuccess',
+  FETCH_TOURNAMENTS_ERROR: 'fetchTournamentsError',
+
+  UPDATE_TOURNAMENT: 'updateTournamentError',
+  UPDATE_TOURNAMENT_ERROR: 'updateTournamentError',
+
+  DELETE_TOURNAMENT: 'deleteTournament',
+  DELETE_TOURNAMENT_ERROR: 'deleteTournamentError',
+
+  QUERY: 'query'
+};
