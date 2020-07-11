d28dfc09a504a5cb0266326b0f550635d8cde4a6
Tamas <jonastamas2@gmail.com>
2020-07-11 13:18:14 +0100

    feat: added Tounament and TournamentList comps

---
+++ src/components/molecules/Tournament/index.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import styled from 'styled-components';
+
+import {
+  deleteTournament,
+  updateTournament
+} from '../../../actions/tournaments';
+import theme from '../../../theme';
+import H6 from '../../atoms/H6';
+import Button from '../../atoms/Button';
+
+const StyledDiv = styled.div`
+  background-color: ${theme.palette.background.base};
+  padding: ${theme.spacing(3)};
+`;
+
+const List = styled.ul`
+  padding: 0;
+  list-style: none;
+  margin-bottom: ${theme.spacing(2)};
+`;
+
+const ButtonEdit = styled(Button)`
+  margin-right: ${theme.spacing(2)};
+`;
+
+const timeOptions = {
+  year: 'numeric',
+  month: 'numeric',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+  second: 'numeric',
+  hour12: false
+};
+
+const Tournament = ({
+  tournament: { id, name, organizer, game, participants, startDate }
+}) => {
+  const dispatch = useDispatch();
+  const startDateFormatted = new Date(startDate).toLocaleDateString(
+    'en-GB',
+    timeOptions
+  );
+
+  return (
+    <StyledDiv>
+      <H6>{name}</H6>
+      <List>
+        <li>Organizer: {organizer}</li>
+        <li>Game: {game}</li>
+        <li>
+          Participants: {participants.current}/{participants.max}
+        </li>
+        <li>Start: {startDateFormatted}</li>
+      </List>
+      <div>
+        <ButtonEdit
+          onClick={() => {
+            const newName = window.prompt('New Tournament Name:', name);
+            dispatch(updateTournament(id, newName));
+          }}
+        >
+          Edit
+        </ButtonEdit>
+        <Button
+          onClick={() =>
+            window.confirm('Do you really want to delete this tournament?') &&
+            dispatch(deleteTournament(id))
+          }
+        >
+          Delete
+        </Button>
+      </div>
+    </StyledDiv>
+  );
+};
+
+export default Tournament;
---
+++ src/components/organisms/TournamentList/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+import theme from '../../../theme';
+import Tournament from '../../molecules/Tournament';
+
+const StyledDiv = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-gap: ${theme.spacing(6)};
+`;
+
+const TournamentList = ({ data }) => {
+  if (data.error) {
+    return <div>Something went wrong.</div>;
+  }
+
+  if (data.loading) {
+    return <div>Lodaing tournaments...</div>;
+  }
+
+  return (
+    <StyledDiv>
+      {data.tournaments &&
+        data.tournaments.map(tournament => (
+          <Tournament tournament={tournament} key={tournament.id} />
+        ))}
+    </StyledDiv>
+  );
+};
+
+export default TournamentList;
